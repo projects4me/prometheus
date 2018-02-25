e0d000f123c1438d89695f1ee0035941b7242559
Hammad Hassan <gollomer@gmail.com>
2018-02-25 08:47:57 -0800

    Adding laneitem opacity

--- tests/integration/helpers/laneitem-color-test.js
+++ tests/integration/helpers/laneitem-color-test.js
@@ -8,12 +8,22 @@ describe('Integration | Helper | laneitem-color', function() {
     integration: true
   });
 
-  it('renders', function() {
+  it('renders for op1', function() {
     this.set('inputValue', '1234');
 
     this.render(hbs`{{laneitem-color inputValue}}`);
 
     expect(this.$().text().trim()).to.equal('border-left-color: rgba(210, 121, 194, 1)');
   });
+
+  it('renders for op1', function() {
+      this.set('a', '1234');
+      this.set('b', '0.5');
+
+      this.render(hbs`{{laneitem-color a b}}`);
+
+      expect(this.$().text().trim()).to.equal('border-left-color: rgba(210, 121, 194, 0.5)');
+  });
+
 });
 
