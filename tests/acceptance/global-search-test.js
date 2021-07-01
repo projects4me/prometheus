69718e88bc4fd867ce12e77e8a033c78b920d38a
nouman <noman.naeem@rolustech.net>
2021-07-01 12:16:23 +0500

    li clicking issue resolved

--- tests/acceptance/global-search-test.js
+++ tests/acceptance/global-search-test.js
@@ -3,7 +3,6 @@ import { visit, currentURL, fillIn, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
-
 module('Acceptance | global search', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
@@ -31,7 +30,7 @@ module('Acceptance | global search', function(hooks) {
     await click('div.global-search > div');
     await fillIn('div.ember-power-select-search > input','3');
     await new Promise(resolve => setTimeout(resolve, 2000));
-    await click('div#ember-basic-dropdown-content-ember349 > ul > li:nth-child(4)');
+    await click('div.ember-power-select-dropdown > ul > li:nth-child(4)');
     console.log(`Current URL is--> ${currentURL()}`);
     await new Promise(resolve => setTimeout(resolve, 2000));
     assert.equal(currentURL(),'/app/project/1/issue/3');
