44dbb4e84b213822e343bfea6465ac32692ff51b
nouman <noman.naeem@rolustech.net>
2021-10-09 12:41:48 +0500

    board controller updated to octane

--- app/controllers/app/project/board.js
+++ app/controllers/app/project/board.js
@@ -4,6 +4,8 @@
 
 import Prometheus from "prometheus/controllers/prometheus";
 import _ from 'lodash';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 /**
  * This is the controller for the board controller
@@ -14,8 +16,8 @@ import _ from 'lodash';
  * @extends Prometheus
  * @author Hammad Hassan <gollomer@gmail.com>
  */
-export default Prometheus.extend({
-
+export default class Board extends Prometheus {
+    @tracked dragFinishText = false;
     /**
      * These are the issues statues
      *
@@ -24,14 +26,14 @@ export default Prometheus.extend({
      * @for Board
      * @public
      */
-    statuses: [
+    statuses = [
         'new',
         'in_progress',
         'done',
         'feedback',
         'pending',
         'deferred'
-    ],
+    ];
 
     /**
      * These are the issues statues
@@ -41,14 +43,14 @@ export default Prometheus.extend({
      * @for Board
      * @public
      */
-    statusClass: {
-        new : 'box-info',
-        in_progress : 'box-primary',
-        done : 'box-success',
-        feedback : 'box-warning',
-        pending : 'box-danger',
-        deferred : ''
-    },
+    statusClass = {
+        new: 'box-info',
+        in_progress: 'box-primary',
+        done: 'box-success',
+        feedback: 'box-warning',
+        pending: 'box-danger',
+        deferred: ''
+    };
 
     /**
      * These are the issues statues
@@ -58,7 +60,7 @@ export default Prometheus.extend({
      * @for Board
      * @public
      */
-    originalIssues : null,
+    originalIssues = null;
 
     /**
      * These are the actions supported by the Project Board View
@@ -68,81 +70,80 @@ export default Prometheus.extend({
      * @for Board
      * @public
      */
-    actions:{
 
-        /**
-         * This action is used to help navigate a user to the detail view of an issue
-         *
-         * @method openIssue
-         * @param App.Model.Issue issue The issue the user wants to navigate to
-         * @public void
-         */
-        openIssue(issue){
-            Logger.debug("AppProjectBoardController::openIssue");
-            this.transitionToRoute('app.project.issue.page',{issue_number:issue.get('issueNumber')});
-            Logger.debug("-AppProjectBoardController::openIssue");
-        },
-
-        /**
-         * This function is responsible for updating the status of an issue
-         *
-         * @method updateIssue
-         * @param {string} issue The issue that needs to be updated
-         * @param {string} el The element
-         * @public
-         */
-        updateIssue(issue,el){
-            Logger.debug("AppProjectBoardController::updateIssue");
-            let _self = this;
-
-            Logger.debug('The issue received is', issue);
-            Logger.debug('The element that was dragged is',el);
-
-            // Update the status in the card
-            let status = el.event.target.parentElement.children[0].getAttribute('data-field-status');
-            issue.set('status',status);
-
-            // We have to save the milestones which contains the card we are updaing for some weird reason
-            let milestones = _self.get('milestones');
-            issue.save();
-            milestones.save();
-
-            Logger.debug("AppProjectBoardController::updateIssue");
-        },
-
-        /**
-         * This function is responsible for filtering the issues based on description and subject
-         *
-         * @method filterIssues
-         * @public
-         */
-        filterIssues(query,el){
-            Logger.debug("AppProjectBoardController::filterIssues");
-            let _self = this;
-            let milestoneId = el.target.dataset.input;
-
-            if (_self.originalIssues == null){
-                _self.originalIssues = _.clone(_self.get('milestones').findBy('id',milestoneId).get('issues')._objects);
-            }
-
-            let issues = _self.originalIssues.filter(function(issue){
-                let subject = issue.get('subject');
-                let description = issue.get('description');
-                let issueNumber = issue.get('issueNumber');
-
-                if ((subject != null && subject.includes(query)) ||
-                    (description != null && description.includes(query)) ||
-                    (issueNumber != null && issueNumber.includes(query))
-                ){
-                    return true;
-                }
-            });
-
-            _self.get('milestones').findBy('id',milestoneId).set('issues',issues);
-
-            Logger.debug("AppProjectBoardController::filterIssues");
-        }
-
+    /**
+     * This action is used to help navigate a user to the detail view of an issue
+     *
+     * @method openIssue
+     * @param App.Model.Issue issue The issue the user wants to navigate to
+     * @public void
+     */
+    @action openIssue(issue) {
+        Logger.debug("AppProjectBoardController::openIssue");
+        this.transitionToRoute('app.project.issue.page', { issue_number: issue.get('issueNumber') });
+        Logger.debug("-AppProjectBoardController::openIssue");
     }
 
-});
+    /**
+     * This function is responsible for updating the status of an issue
+     *
+     * @method updateIssue
+     * @param {string} issue The issue that needs to be updated
+     * @param {string} el The element
+     * @public
+     */
+    @action updateIssue(issue, el) {
+        debugger;
+        Logger.debug("AppProjectBoardController::updateIssue");
+        let _self = this;
+        this.dragFinishText = el.target.resultText;
+        Logger.debug('The issue received is', issue);
+        Logger.debug('The element that was dragged is', el);
+
+        // Update the status in the card
+        let status = el.event.target.parentElement.children[0].getAttribute('data-field-status');
+        issue.set('status', status);
+
+        // We have to save the milestones which contains the card we are updaing for some weird reason
+        let milestones = _self.get('milestones');
+        issue.save();
+        milestones.save();
+
+        Logger.debug("AppProjectBoardController::updateIssue");
+    }
+
+    /**
+     * This function is responsible for filtering the issues based on description and subject
+     *
+     * @method filterIssues
+     * @public
+     */
+    @action filterIssues(query, el) {
+        Logger.debug("AppProjectBoardController::filterIssues");
+        let _self = this;
+        let milestoneId = el.target.dataset.input;
+
+        if (_self.originalIssues == null) {
+            _self.originalIssues = _.clone(_self.get('milestones').findBy('id', milestoneId).get('issues')._objects);
+        }
+
+        let issues = _self.originalIssues.filter(function (issue) {
+            let subject = issue.get('subject');
+            let description = issue.get('description');
+            let issueNumber = issue.get('issueNumber');
+
+            if ((subject != null && subject.includes(query)) ||
+                (description != null && description.includes(query)) ||
+                (issueNumber != null && issueNumber.includes(query))
+            ) {
+                return true;
+            }
+        });
+
+        _self.get('milestones').findBy('id', milestoneId).set('issues', issues);
+
+        Logger.debug("AppProjectBoardController::filterIssues");
+    }
+
+
+}
