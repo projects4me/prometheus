ce3e41d15d89e2a6fc7bc8d6a118bf778c4e1565
nouman <noman.naeem@rolustech.net>
2021-11-11 14:54:24 +0500

    commenting added

--- app/modifiers/initialize-sortable.js
+++ app/modifiers/initialize-sortable.js
@@ -5,6 +5,9 @@ import Sortable from 'sortablejs';
  * This modifier is called on the initialization of taskboard component and SortableJS
  * is attached to every items inside that taskboard in order to make it draggable.
  *
+ * @example
+ *      <div id="taskboard-container" {{initialize-sortable group="milestone" ...}}>
+ * 
  * @class InitializeSortable
  * @namespace Prometheus.Modifiers
  * @extends Modifier
@@ -13,7 +16,9 @@ import Sortable from 'sortablejs';
 export default class InitializeSortable extends Modifier {
 
     /**
-     * This function returns group name.
+     * This function returns group name. This property is attached with every lane of taskboard.
+     * An item can be switched from one lane to other if the other lane, on which we are dropping
+     * our item, has same group as the dragged item's lane have.
      *
      * @method get
      * @return String
@@ -38,7 +43,8 @@ export default class InitializeSortable extends Modifier {
 
     /**
      * This function returns scroll sensitivty in px. The value shows how 
-     * near the mouse will be to an edge to start scrolling.
+     * near the mouse will be to an edge to start scrolling. This will work
+     * if forceFallBack value is set to true.
      *
      * @method get
      * @return integer
@@ -49,7 +55,8 @@ export default class InitializeSortable extends Modifier {
     }
 
     /**
-     * This function returns scrolling speed in px.
+     * This function returns scrolling speed in px. This will work if forceFallBack value
+     * is set to true.
      *
      * @method get
      * @return integer
@@ -60,7 +67,8 @@ export default class InitializeSortable extends Modifier {
     }
 
     /**
-     * This function returns sort value. If set to true then items will be sorted.
+     * This function returns sort value. If set to true then sorting of items between lanes
+     * will be enabled.
      *
      * @method get
      * @return boolean
@@ -128,7 +136,9 @@ export default class InitializeSortable extends Modifier {
     }
 
     /**
-     * This property contains array of sortable objects.
+     * This property contains array of sortable objects. This is used to store sortable
+     * objects that are attached to each lane. This property is used when this page will 
+     * destroy in order to remove sortable from each lane.
      *
      * @property sortableList
      * @type Array
@@ -180,6 +190,7 @@ export default class InitializeSortable extends Modifier {
     /**
      * This function is bind with onStart function of the sortablejs. This function
      * will be triggered when user will start the dragging of an item from taskboard.
+     * 
      * @method _onStart
      * @param {Object} evt 
      */
@@ -191,6 +202,7 @@ export default class InitializeSortable extends Modifier {
     /**
      * This function is bind with onEnd function of the sortablejs. This function
      * will be triggered when dragging of an item is stopped by user.
+     * 
      * @method _onEnd
      * @param {Object} evt 
      */
@@ -202,8 +214,11 @@ export default class InitializeSortable extends Modifier {
     }
 
     /**
-     * This function highlight dropzones related to current selected element and
-     * attach mouse enter and leave event to that element.
+     * This function highlight dropzones related to current selected item. It is called on 'onStart'
+     * function of the sortablejs and that function is called when user just starts the dragging
+     * of an item from a lane and the remaining lanes on the taskboard having same group name, 
+     * which the dragged item's lane have, will be highlighted.
+     * 
      * @method selectDropzones
      * @param {Object} evt 
      */
@@ -217,8 +232,10 @@ export default class InitializeSortable extends Modifier {
     }
     
     /**
-     * This function removes highlighted dropzones related to current selected element and
-     * removes mouse enter and leave event to that element.
+     * This function removes highlighted dropzones related to current selected item. It is called on 'onEnd'
+     * function of the sortablejs and that function is called when user just stops the dragging of an
+     * item.
+     * 
      * @method unSelectDropzones
      * @param {Object} evt 
      * @param {HTMLElement} el 
@@ -234,9 +251,11 @@ export default class InitializeSortable extends Modifier {
     }
 
     /**
-     * This function is called when an item of one lane is shifted to another lane
-     * and is used to set the height of parent element by getting max size of
-     * lane and set it to its parent element.
+     * This function is called when an item of one lane is shifted to another lane (onEnd function of sortablejs)
+     * and is used to set the height of parent element by getting max size of a lane and that max is set it to its
+     * parent element. Parent element is a section which contain lanes. Firstly this function will be called on the 
+     * initialization of this modifier (on didInstall function) to set the every sections of milestone.
+     * 
      * @method setParentHeight
      */
     setParentHeight() {
