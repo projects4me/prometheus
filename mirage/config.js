f923aaa8e3b3adbdbc09d37fb6cce12d8906cda2
nouman <noman.naeem@rolustech.net>
2022-01-17 18:07:43 +0500

    some changes in mirage's config file

--- mirage/config.js
+++ mirage/config.js
@@ -1,15 +1,15 @@
 import _ from "lodash";
 
-export default function() {
+export default function () {
     this.urlPrefix = 'http://projects4me';
     this.namespace = '/api/v1';
     this.timing = 0;
 
     this.get('/user', (schema) => {
-        let data =  schema.users.all();
+        let data = schema.users.all();
         let count = 1;
-        let model =  { data: []};
-        _.each(data.models,function(obj){
+        let model = { data: [] };
+        _.each(data.models, function (obj) {
             model.data.push({
                 type: 'User',
                 id: count++,
@@ -20,9 +20,8 @@ export default function() {
     });
 
     this.get('/user/:id', (schema, request) => {
-        // debugger;
         let id = request.params.id;
-        if (id === "me"){
+        if (id === "me") {
             id = 1;
         }
 
@@ -40,7 +39,7 @@ export default function() {
                     }
                 }
             },
-            included:[
+            included: [
                 {
                     type: 'dashboard',
                     id: id,
@@ -48,97 +47,20 @@ export default function() {
                 }
             ]
         };
-        if(_.has(server, 'customUser')){
-            data = server.customUser(schema,request)
+        if (_.has(server, 'customUser')) {
+            data = server.customUser(schema, request)
         }
         return data;
     });
 
-    this.get('/issue', (schema) => {
-        let data =  schema.issues.all();
-        let count = 1;
-        let id = 1;
-        let model =  { data: [], included: []};
-        _.each(data.models,function(obj){
-            model.data.push({
-                type: 'issue',
-                id: count++,
-                attributes: obj.attrs,
-                relationships: {
-                    assignedTo: {
-                        data:{
-                            type: "user",
-                            id: id
-                        }
-                    },
-                    createdBy: {
-                        data: {
-                            type: "user",
-                            id: id
-                        }
-                    },
-                    modifiedBy: {
-                        data: {
-                            type: "user",
-                            id: id
-                        }
-                    },
-                    ownedBy: {
-                        data: {
-                            type: "user",
-                            id: id
-                        }
-                    },
-                    reportedBy: {
-                        data: {
-                            type: "user",
-                            id: id
-                        }
-                    },
-                    project: {
-                        data: {
-                            type: "project",
-                            id: id
-                        }
-                    },
-                    milestone: {
-                        data: {
-                            type: "milestone",
-                            id: id
-                        }
-                    },
-                    issuetype: {
-                        data: {
-                            type: "issuetype",
-                            id: id
-                        }
-                    }
-                }
-            });
-        });
-
-        model.included = [
-            {
-                type: 'user',
-                id: id,
-                attributes: schema.users.find(id).attrs,
-            },
-            {
-                type: 'project',
-                id: id,
-                attributes: schema.projects.find(id).attrs,
-            },
-            {
-                type: 'milestone',
-                id: id,
-                attributes: schema.milestones.find(id).attrs,
-            },
-            {
-                type: 'issuetype',
-                id: id,
-                attributes: schema.issuetypes.find(id).attrs,
+    this.get('/issue', (schema, request) => {
+        let model = schema.issues.all();
+        let queryParams = request.queryParams.query;
+        if (queryParams == '(((Issue.milestoneId NULL) OR (Issue.milestoneId EMPTY)) AND (Issue.projectId : 10))') {
+            if (_.has(server, 'customDataIssue')) {
+                model = server.customDataIssue(schema);
             }
-        ];
+        }
 
         return model;
     });
@@ -152,7 +74,7 @@ export default function() {
                 attributes: schema.issues.find(id),
                 relationships: {
                     assignedTo: {
-                        data:{
+                        data: {
                             type: "user",
                             id: id
                         }
@@ -201,7 +123,7 @@ export default function() {
                     }
                 }
             },
-            included : [
+            included: [
                 {
                     type: 'user',
                     id: id,
@@ -236,7 +158,7 @@ export default function() {
                 attributes: schema.issues.find(id),
                 relationships: {
                     assignedTo: {
-                        data:{
+                        data: {
                             type: "user",
                             id: id
                         }
@@ -285,7 +207,7 @@ export default function() {
                     }
                 }
             },
-            included : [
+            included: [
                 {
                     type: 'user',
                     id: id,
@@ -311,10 +233,10 @@ export default function() {
     });
 
     this.get('/role', (schema) => {
-        let data =  schema.roles.all();
+        let data = schema.roles.all();
         let count = 1;
-        let model =  { data: []};
-        _.each(data.models,function(obj){
+        let model = { data: [] };
+        _.each(data.models, function (obj) {
             model.data.push({
                 type: 'role',
                 id: count++,
@@ -324,119 +246,32 @@ export default function() {
         return model;
     });
 
-    this.get('/milestone', (schema) => {
-        let data =  schema.milestones.all();
-        let count = 1;
-        let model =  { data: []};
-        _.each(data.models,function(obj){
-            model.data.push({
-                type: 'milestone',
-                id: count++,
-                attributes: obj.attrs
-            });
-        });
+    this.get('/milestone', (schema, request) => {
+        let model = schema.milestones.all();
+        // let queryParams = request.queryParams.query;
+        // if(queryParams == '(Milestone.projectId : 3 )') {
+        //     model = schema.projects.find(3).milestones;
+        // }
         return model;
     });
 
     this.get('/project', (schema, request) => {
-        let model =  schema.projects.all();
+        let model = schema.projects.all();
         let queryParams = request.queryParams.query;
-        
-        if(_.has(server, 'customDataProject')){
-            server.customDataProject(schema,request)
-        }
-        if (queryParams == "(Project.id : 3)") {
-            let projectId = queryParams.replace(/\)/g, "").replace(/^\D+/g, "");
-            model = { data : []};
-            let data = schema.projects.find(projectId);
-            model.data.push({data , type: "project", id: "3"});
-            return model;
-        }
+        // if(_.has(server, 'customDataProject')){
+        //     server.customDataProject(schema,request)
+        // }
+        // if (queryParams == "(Project.id : 3)") {
+        //     let projectId = queryParams.replace(/\)/g, "").replace(/^\D+/g, "");
+        //     model = { data : []};
+        //     let data = schema.projects.find(projectId);
+        //     model.data.push({data , type: "project", id: "3"});
+        // }
         return model;
     });
 
     this.get('/project/:id', (schema, request) => {
         let id = request.params.id;
-        // debugger;
-        // let data = {
-        //     data: {
-        //         type: 'project',
-        //         id: id,
-        //         attributes: schema.projects.find(id).attrs,
-        //         relationships: {
-        //             members: {
-        //                 data: [
-        //                     {
-        //                         type: "user",
-        //                         id: id
-        //                     }
-        //                 ]
-        //             },
-        //             memberships: {
-        //                 data: [
-        //                     {
-        //                         type: "membership",
-        //                         id: id
-        //                     }
-        //                 ]
-        //             },
-        //             milestones: {
-        //                 data: [
-        //                     {
-        //                         type: "milestone",
-        //                         id: id
-        //                     }
-        //                 ]
-        //             },
-        //             issuetypes: {
-        //                 data: [
-        //                     {
-        //                         type: "issuetype",
-        //                         id: id
-        //                     }
-        //                 ]
-        //             },
-        //             roles: {
-        //                 data: [
-        //                     {
-        //                         type: "role",
-        //                         id: id
-        //                     }
-        //                 ]
-        //             }
-        //         }
-        //     },
-        //     included:[
-        //         {
-        //             type: 'user',
-        //             id: id,
-        //             attributes: schema.users.find(id).attrs,
-        //         },
-        //         {
-        //             type: 'membership',
-        //             id: id,
-        //             attributes: schema.memberships.find(id).attrs,
-        //         },
-        //         {
-        //             type: 'milestone',
-        //             id: id,
-        //             attributes: schema.milestones.find(id).attrs,
-        //         },
-        //         {
-        //             type: 'issuetype',
-        //             id: id,
-        //             attributes: schema.issuetypes.find(id).attrs,
-        //         },
-        //         {
-        //             type: 'role',
-        //             id: id,
-        //             attributes: schema.roles.find(id).attrs,
-        //         },
-        //     ]
-        // };
-        // debugger
-        // return data;
-        // debugger;
         return schema.projects.find(id)
     });
 
@@ -473,4 +308,13 @@ export default function() {
         }
     });
 
+    this.patch('/issue/:id', (schema, request) => {
+        let requestData = JSON.parse(request.requestBody).data;
+        let issue = schema.issues.find(requestData.id);
+        issue.update({
+            status: requestData.attributes.status,
+            milestoneId: requestData.attributes.milestoneId
+        });
+        return issue;
+    });
 }
