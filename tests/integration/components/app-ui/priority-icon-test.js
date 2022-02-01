541fc44898abef7029abdd38f77dc05d8ae13273
nouman <noman.naeem@rolustech.net>
2022-02-01 14:42:54 +0500

    adding intl hook

--- tests/integration/components/app-ui/priority-icon-test.js
+++ tests/integration/components/app-ui/priority-icon-test.js
@@ -6,9 +6,11 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { setupIntl } from 'ember-intl/test-support';
 
 module('Integration | Component | app-ui/priority-icon', function (hooks) {
     setupRenderingTest(hooks);
+    setupIntl(hooks, 'en-us');
 
     test('Priority Icon Renders', async function (assert) {
 
