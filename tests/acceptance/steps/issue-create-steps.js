b76bf1fb7a912d54dd104a819ea2a386f4e3c756
nouman <noman.naeem@rolustech.net>
2022-02-04 23:18:15 +0500

    issue create test added

--- tests/acceptance/steps/issue-create-steps.js
+++ tests/acceptance/steps/issue-create-steps.js
@@ -1,54 +1,127 @@
-import { fillIn, currentURL, visit } from '@ember/test-helpers';
+import { fillIn, currentURL, visit, click } from '@ember/test-helpers';
 import { clickTrigger, selectChoose } from 'ember-power-select/test-support/helpers';
-
 import steps from './steps';
 
 export const given = function () {
-    return [{
-        "Project $projectId has $milestoneCount milestones": (assert) => async function (projectId, milestoneCount) {
-            let project = server.schema.projects.find(projectId);
-            assert.equal(project.milestoneIds.length, milestoneCount, `Project ${projectId} has ${milestoneCount} milestones`);
+    return [
+        {
+            "$userName selects Project $projectId": (assert, ctx) => async function (userName, projectId) {
+                let project = server.create('project');
+                let oldProjectId = project.id;
+                let users = server.schema.users.all().models;
+                project.update({
+                    id: projectId,
+                    members: users
+                });
+                server.db.projects.remove(oldProjectId);
+                ctx.set('currentProject', project);
+                assert.equal(project.id, projectId);
+            }
+        },
+        {
+            "Project $projectId has following details\n$table": (assert, ctx) => async function (projectId, table) {
+                let project = ctx.get('currentProject');
+                _setProject(table[0], project);
+            }
         }
-    }]
+    ];
 }
 
 export const when = function () {
-    return [{
-        "User navigates to issue create page": (assert) => async function () {
-            await visit('/app/project/3/issue/create/');
-            assert.equal(currentURL(), '/app/project/3/issue/create/', `User navigates to issue create page`);
+    return [
+        {
+            "User navigates to issue create page": (assert, ctx) => async function () {
+                let currentProject = ctx.get('currentProject');
+                await visit(`/app/project/${currentProject.id}/issue/create/`);
+                assert.equal(currentURL(), `/app/project/${currentProject.id}/issue/create/`, `User navigates to issue create page`);
+            },
         },
-    }, {
-        "User enters subject": (assert) => async function () {
-            let inputElement = document.querySelector('div.form-group > div[data-field="issue.subject"] > input');
-            await fillIn(inputElement, "test subject");
-            assert.ok(true, "User enters subject");
+        {
+            "User enters $content in subject": (assert) => async function (content) {
+                let inputElement = document.querySelector('div.form-group > div[data-field="issue.subject"] > input');
+                await fillIn(inputElement, content);
+                assert.ok(true, "User enters subject");
+            }
+        },
+        {
+            "User enters $content in description": (assert) => async function (content) {
+                let inputElement = document.querySelector('div[data-field="issue.description"]  div.tui-editor-contents.tui-editor-contents-placeholder');
+                await fillIn(inputElement, content);
+                assert.ok(true, "User enters description");
+            }
+        },
+        {
+            "User selects option $id from type": (assert) => async function (id) {
+                await clickTrigger('div[data-field="issue.type"] > div.input-group.select-input');
+                await selectChoose('div[data-field="issue.type"] > div.input-group.select-input > div', '.ember-power-select-option', id - 1);
+                assert.ok(true, "User selects type");
+            }
+        },
+        {
+            "User selects status": (assert) => async function () {
+                await clickTrigger('div[data-field="issue.status"] > div.input-group.select-input');
+                await selectChoose('div[data-field="issue.status"] > div.input-group.select-input > div', '.ember-power-select-option', 1);
+                assert.ok(true, "User selects status");
+            }
+        },
+        {
+            "User selects option $id from milestone": (assert) => async function (id) {
+                await clickTrigger('div[data-field="issue.milestone"] > div.input-group.select-input');
+                await selectChoose('div[data-field="issue.milestone"] > div.input-group.select-input > div', '.ember-power-select-option', id); //not subtracted by 1 because it has it default value
+                assert.ok(true, "User selects milestone");
+            }
+        },
+        {
+            "User clicks on save button": (assert) => async function () {
+                let btn = document.querySelector('button#btn-save');
+                await click(btn);
+                assert.ok(true, "User clicks on save button");
+            }
         }
-    }, {
-        "User enters description": (assert) => async function () {
-            let inputElement = document.querySelector('div[data-field="issue.description"]  div.tui-editor-contents.tui-editor-contents-placeholder');
-            await fillIn(inputElement, "test description");
-            assert.ok(true, "User enters description");
-        }
-    }, {
-        "User selects type": (assert) => async function () {
-            await clickTrigger('div[data-field="issue.type"] > div.input-group.select-input');
-            await selectChoose('div[data-field="issue.type"] > div.input-group.select-input > div', '.ember-power-select-option', 3);
-            assert.ok(true, "User selects type");
-        }
-    }
-    ]
+    ];
 }
 
 export const then = function () {
-    return [{
-        "$userName should be $field": (assert) => async function (userName, field) {
-            let assigneeElement = document.querySelector(`div[data-field="issue.${field}"] span.ember-power-select-selected-item span.username`);
-            assert.equal(assigneeElement.innerText, userName, `${userName} should be ${field}`);
+    return [
+        {
+            "$userName should be $field": (assert) => async function (userName, field) {
+                let selector = `div[data-field='issue.${field}'] span.ember-power-select-selected-item span.username`;
+                let assigneeElement = document.querySelector(selector);
+                assert.equal(assigneeElement.innerText, userName, `${userName} should be ${field}`);
+            }
+        },
+        {
+            "User is navigated to issue detail view": (assert, ctx) => async function () {
+                let currentProject = ctx.get('currentProject');
+                let LatestCreatedIssue = ctx.get('latestCreatedIssue');
+                assert.equal(currentURL(), `/app/project/${currentProject.id}/issue/${LatestCreatedIssue.id}`, 'url matched');
+            }
+        },
+        {
+            "Issue subject is $content": (assert) => async function (content) {
+                let el = document.querySelector('div[data-field="issue.subject"] span.issueSubject');
+                assert.equal(content, el.innerText, 'subject matched');
+            }
+        },
+        {
+            "Issue description is $content": (assert) => async function (content) {
+                let el = document.querySelector('div[data-field="issue.description"] > p');
+                assert.equal(content, el.innerText, 'description matched');
+            }
         }
-    }]
+    ];
 }
 
 export default function (assert) {
     return steps(assert);
+}
+
+function _setProject(projectDetails, project) {
+    for (const [module, count] of Object.entries(projectDetails)) {
+        let rels = server.createList(module, parseInt(count));
+        let relName = `${module}s`;
+        project.update({
+            [relName]: rels
+        });
+    }
 }
