ef097ddda096c65cee1f51d91cdcc991315430a8
nouman <noman.naeem@rolustech.net>
2022-02-15 16:52:36 +0500

    adding provider component for issues rendering

---
+++ app/components/app-ui/task-board/issues-provider.js
@@ -0,0 +1,31 @@
+/*
+ * Projects4Me Copyright (c) 2017. Licensing : http://legal.projects4.me/LICENSE.txt. Do not remove this line
+ */
+
+import Component from '@glimmer/component';
+
+/**
+ * This component is used to render issues, related to the milestone.
+ *
+ * @class TaskBoardIssuesProvider
+ * @namespace Prometheus.Components
+ * @extends Ember.Component
+ * @author Rana Nouman <ranamnouman@yahoo.com>
+ */
+export default class TaskBoardIssuesProviderComponent extends Component {
+
+    /**
+     * This function is used to filter the issues based upon user's input and return
+     * that list of filtered issues in order to render them template.
+     *
+     * @method get
+     * @public
+     */
+    get results() {
+        let { issues, query } = this.args;
+        if (query) {
+            issues = issues.filter((issue) => issue.subject.includes(query));
+        }
+        return issues;
+    }
+}
