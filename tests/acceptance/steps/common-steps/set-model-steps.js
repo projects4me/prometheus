8d18f6836f4b37eee79937da125d1e2d233ac7f4
nouman <ranamnouman@yahoo.com>
2022-08-02 12:21:15 +0500

    no need to create extra helper[1]

--- tests/acceptance/steps/common-steps/set-model-steps.js
+++ tests/acceptance/steps/common-steps/set-model-steps.js
@@ -1,4 +1,4 @@
-import { setModel } from '../../../helpers/set-details-of-model';
+import { pluralize } from 'ember-inflector';
 import steps from '../steps';
 
 export const given = function () {
@@ -15,4 +15,37 @@ export const given = function () {
 
 export default function (assert) {
     return steps(assert);
+}
+
+function setModel(modelDetails, model) {
+    for (const [fieldName, value] of Object.entries(modelDetails)) {
+        if (!isNaN(value)) {
+            var [relName, relatedModelName] = getRelatedModelName(fieldName);
+            var relValue = server.createList(relatedModelName, parseInt(value));
+        } else {
+            relName = fieldName;
+            relValue = value;
+        }
+        model.update({
+            [relName]: relValue
+        });
+    }
+}
+
+function getRelatedModelName(fieldName) {
+    let relName = '';
+    let relatedModelName = '';
+    let regex = /\([^)]*\)/g;
+
+    //check if user has given relationship name as input or not
+    if (regex.test(fieldName)) {
+        let splittedString = fieldName.split('(');
+        relName = splittedString[0];
+        relatedModelName = splittedString[1].slice(0, -1);
+    } else {
+        relatedModelName = fieldName;
+        relName = pluralize(fieldName);
+    }
+
+    return [relName, relatedModelName];
 }
