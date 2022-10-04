df14ed178aef86f8fa0f5f2b4d32d6637a65f916
nouman <ranamnouman@yahoo.com>
2022-10-04 16:06:09 +0500

    component to display exception added

---
+++ app/components/exceptions/nothing-found.js
@@ -0,0 +1,16 @@
+/*
+ * Projects4Me Copyright (c) 2017. Licensing : http://legal.projects4.me/LICENSE.txt. Do not remove this line
+ */
+
+import Component from '@glimmer/component';
+
+/**
+ * This component is used to an exception, if there is no content to display.
+ *
+ * @class ExceptionsNothingFoundComponent
+ * @namespace Prometheus.Components
+ * @extends Ember.Component
+ * @author Rana Nouman <ranamnouman@yahoo.com>
+ */
+export default class ExceptionsNothingFoundComponent extends Component {
+}
