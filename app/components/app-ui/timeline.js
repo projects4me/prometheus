fb3f065b5868c10dca026d55a01e8f5cd0c5898c
Noman Naeem <ranamnouman@gmail.com>
2023-02-28 19:33:47 +0500

    comment added on class

--- app/components/app-ui/timeline.js
+++ app/components/app-ui/timeline.js
@@ -1,4 +1,16 @@
+/*
+ * Projects4Me Copyright (c) 2017. Licensing : http://legal.projects4.me/LICENSE.txt. Do not remove this line
+ */
+
 import Component from '@glimmer/component';
 
+/**
+ * This component is used to render all activities performed on modules 
+ * e.g project is created.
+ * @class Timeline
+ * @namespace Prometheus.Components
+ * @extends Ember.Component
+ * @author Rana Nouman <ranamnouman@yahoo.com>
+ */
 export default class AppUiTimelineComponent extends Component {
 }
