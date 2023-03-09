dafb6ce6597788583d557b2916e3d1392bc3e439
Noman Naeem <ranamnouman@gmail.com>
2023-03-09 14:47:30 +0500

    Add `checkbox` component `glimmer based`

---
+++ app/components/form-fields/field-checkbox.js
@@ -0,0 +1,16 @@
+/*
+ * Projects4Me Copyright (c) 2017. Licensing : http://legal.projects4.me/LICENSE.txt. Do not remove this line
+ */
+
+import Component from '@glimmer/component';
+
+/**
+ * This component renders checkbox.
+ *
+ * @class FieldCheckbox
+ * @namespace Prometheus.Components
+ * @extends Component
+ * @author Rana Nouman <ranamnouman@gmail.com>
+ */
+export default class FieldCheckboxComponent extends Component {
+}
