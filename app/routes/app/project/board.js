dafee44d6217d73fd3d39b5a5ab099cf473f4834
Noman Naeem <ranamnouman@gmail.com>
2023-03-14 16:48:01 +0500

    Fetch `issue statuses` that are related to current `project` in TaskBoard

--- app/routes/app/project/board.js
+++ app/routes/app/project/board.js
@@ -3,6 +3,7 @@
  */
 
 import App from "prometheus/routes/app";
+import { hash } from 'rsvp';
 
 /**
  * This is the route to load the task board for a project
@@ -26,18 +27,28 @@ export default App.extend({
     async model() {
         let _self = this;
         let projectId = this.paramsFor('app.project').project_id;
+
         let _milestoneOptions = {
             query: `(Milestone.projectId : ${projectId} )`,
             order: 'Milestone.endDate',
             rels: 'issues',
             limit: -1
         };
+
         let _issueOptions = {
             query: `(((Issue.milestoneId NULL) OR (Issue.milestoneId EMPTY)) AND (Issue.projectId : ${projectId}))`,
             limit: -1
         }
+
+        let _issueStatusOptions = {
+            query: `(Issuestatus.projectId : ${projectId})`,
+            limit: -1
+        };
+
         let milestones = await _self.store.query('milestone', _milestoneOptions);
         let backlogIssues = await _self.store.query('issue', _issueOptions);
+        let issueStatuses = await _self.store.query('issuestatus', _issueStatusOptions);
+
         let backlog = _self.store.createRecord('milestone', {
             id: null,
             milestoneType: "backlog",
@@ -49,9 +60,15 @@ export default App.extend({
 
         milestones.forEach((milestone) => {
             milestonesArray.pushObject(milestone);
-        })
+        });
         milestonesArray.pushObject(backlog);
-        return milestonesArray;
+
+        let model = hash({
+            milestones: milestonesArray,
+            issueStatuses: issueStatuses
+        });
+
+        return model;
     },
 
     /**
@@ -64,6 +81,7 @@ export default App.extend({
      */
     setupController: function (controller, model) {
         Logger.debug('Prometheus.Routes.Board::setupController');
-        controller.set('milestones', model);
+        controller.set('milestones', model.milestones);
+        controller.set('issueStatuses', model.issueStatuses);
     },
 });
