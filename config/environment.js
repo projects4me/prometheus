ebb61d9089ad91f38411b443f63b850b1a023d4d
Noman Naeem <ranamnouman@gmail.com>
2023-03-21 19:32:34 +0500

    check created env vars

--- config/environment.js
+++ config/environment.js
@@ -86,9 +86,7 @@ module.exports = function(environment) {
     }
 
     if (environment === 'production') {
-        console.log('*****************API_HOST*********************');
-        console.log(process);
-        ENV.api.host = process.env;
+        ENV.api.host = process.env.API_HOST_PRODUCTION;;
     }
     return ENV;
 };
