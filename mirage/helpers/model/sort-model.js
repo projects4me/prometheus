28744814f7c328138948fc564ec212196988f2af
Noman Naeem <ranamnouman@gmail.com>
2023-05-23 18:58:48 +0500

    Add helper function that handles sorting of model in testing

---
+++ mirage/helpers/model/sort-model.js
@@ -0,0 +1,44 @@
+/**
+ * This function is used to sort the array of model on the basis of given sort key, order and datatype of sort.
+ * 
+ * @param {String} sort 
+ * @param {String} order 
+ * @param {Array} model 
+ * @param {String} sortDataType The data type of the property to be sorted e.g string, number and string with numbers.
+ * @returns Array
+ */
+export default function sortModel(sort, order = 'asc', model, sortDataType) {
+    if (!_.isEmpty(sort)) {
+        let attribute = sort.split('.').pop();
+        order = order.toLowerCase();
+        let sortStringCb = (a, b) => {
+            return (order === 'desc')
+                ? a[attribute].localeCompare(b[attribute])
+                : b[attribute].localeCompare(a[attribute])
+        };
+
+        let sortNumberCb = (a, b) => {
+            return (order === 'desc')
+                ? b[attribute] - a[attribute]
+                : a[attribute] - b[attribute];
+        }
+
+        let sortStringWithNumberCb = (a, b) => {
+            return order === 'desc'
+                ? (Number(b[attribute].match(/(\d+)/g)[0]) - Number((a[attribute].match(/(\d+)/g)[0])))
+                : (Number(a[attribute].match(/(\d+)/g)[0]) - Number((b[attribute].match(/(\d+)/g)[0])))
+        }
+
+
+        if (sortDataType === 'string') {
+            return model.sort(sortStringCb);
+        } else if (sortDataType === 'number') {
+            return model.sort(sortNumberCb)
+        } else if (sortDataType === 'stringWithNumber') {
+            debugger;
+            return model.sort(sortStringWithNumberCb);
+        }
+
+    }
+    return model;
+}
