1391673a51cb13ee96ac050baa2bd23f11e355f8
Noman Naeem <ranamnouman@gmail.com>
2023-06-16 18:21:07 +0500

    Indent

--- app/controllers/app/user/create.js
+++ app/controllers/app/user/create.js
@@ -86,7 +86,7 @@ export default class AppUserCreateController extends PrometheusCreateController 
                 custom: [
                     {
                         action: "checkUsernameAvailabilityTask"
-                    }                    
+                    }
                 ]
             }
         },
