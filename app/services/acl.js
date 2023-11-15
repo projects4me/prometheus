43bfe1563bd107104a7da4f2f352ba27724d4a60
Noman Naeem <ranamnouman@gmail.com>
2023-11-15 19:36:56 +0500

    Add route maps against acl resources

--- app/services/acl.js
+++ app/services/acl.js
@@ -32,18 +32,106 @@ export default class AclService extends Service {
      */
     routeMaps = [
         {
-            name: 'app.index',
-            map: 'App'
+            name: 'app.index', // the route name, user is transiting to.
+            map: 'App' // acl map
         },
+        {
+            name: 'app.projects.index',
+            map: 'Route.Project.List'
+        },
+        {
+            name: 'app.projects.create',
+            map: 'Route.Project.Create'
+        },
+        {
+            name: 'app.projects.edit',
+            map: 'Route.Project.Edit'
+        },
+        {
+            name: 'app.project.index',
+            map: 'Route.Project.Read'
+        },
+        {
+            name: 'app.project.wiki.index',
+            map: 'Route.Wiki.List'
+        },
+        {
+            name: 'app.project.wiki.create',
+            map: 'Route.Wiki.Create'
+        },
+        {
+            name: 'app.project.wiki.page',
+            map: 'Route.Wiki.Read'
+        },
+        {
+            name: 'app.project.wiki.edit',
+            map: 'Route.Wiki.Edit'
+        },
+        {
+            name: 'app.project.conversation',
+            map: 'Route.Project.Conversation'
+        },        
         {
             name: 'app.project.board',
-            map: 'App.Project.Board'
+            map: 'Route.Project.Board'
         },
         {
-            name: 'app.access-denied',
-            map: 'App.AccessDenied'
+            name: 'app.project.calendar',
+            map: 'Route.Project.Calendar'
+        },
+        {
+            name: 'app.project.issue.index',
+            map: 'Route.Issue.List'
+        },
+        {
+            name: 'app.project.issue.create',
+            map: 'Route.Issue.Create'
+        },
+        {
+            name: 'app.project.issue.page',
+            map: 'Route.Issue.Read'
+        },
+        {
+            name: 'app.project.issue.edit',
+            map: 'Route.Issue.Edit'
+        },
+        {
+            name: 'app.user.page',
+            map: 'Route.User.Read'
+        },
+        {
+            name: 'app.user.create',
+            map: 'Route.User.Create'
+        },
+        {
+            name: 'app.user.index',
+            map: 'Route.User.List'
+        },
+        {
+            name: 'app.user.edit',
+            map: 'Route.User.Edit'
+        },
+        {
+            name: 'app.user.management',
+            map: 'Route.User.Management'
+        },
+        {
+            name: 'app.admin.index',
+            map: 'Route.Admin.List'
+        },
+        {
+            name: 'app.admin.create',
+            map: 'Route.Admin.Create'
+        },
+        {
+            name: 'app.admin.page',
+            map: 'Route.Admin.Read'
+        },
+        {
+            name: 'app.admin.edit',
+            map: 'Route.Admin.Edit'
         }
-    ]
+    ];
 
     /**
      * This method return list of the resources of the application on which 
@@ -69,7 +157,6 @@ export default class AclService extends Service {
     hasRouteAccess(routeName) {
         let route = this.routeMaps.find(f => f.name === routeName);
         
-        // return this.allowedResources.includes(route.map);
-        return true;
+        return this.allowedResources.includes(route.map);
     }
 }
