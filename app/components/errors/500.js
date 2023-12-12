53d6c83b2fc594e575d30810493fb29e406940f0
Noman Naeem <ranamnouman@gmail.com>
2023-12-12 15:23:26 +0500

    Add different type of error components

---
+++ app/components/errors/500.js
@@ -0,0 +1,15 @@
+/*
+ * Projects4Me Copyright (c) 2017. Licensing : http://legal.projects4.me/LICENSE.txt. Do not remove this line
+ */
+import Component from '@glimmer/component';
+
+/**
+ * This component is rendered when server returns 500 internal server error.
+ *
+ * @class Errors500Component
+ * @namespace Prometheus.Components
+ * @extends Ember.Component
+ * @author Rana Nouman <ranamnouman@gmail.com>
+ */
+export default class Errors500Component extends Component {
+}
