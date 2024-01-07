dc54b000dca41be6805a6d3a294af091e84f418d
Noman Naeem <ranamnouman@gmail.com>
2024-01-08 03:19:57 +0500

    New route `role/page` added

---
+++ app/controllers/app/role/page.js
@@ -0,0 +1,16 @@
+/*
+ * Projects4Me Copyright (c) 2017. Licensing : http://legal.projects4.me/LICENSE.txt. Do not remove this line
+ */
+
+import AppRoleController from 'prometheus/controllers/app/role';
+
+/**
+ * The role page controller.
+ *
+ * @class AppRolePageController
+ * @namespace Prometheus.Controller
+ * @extends Ember.Controller
+ * @author Rana Nouman <ranamnouman@gmail.com>
+ */
+export default class AppRolePageController extends AppRoleController {
+}
---
+++ app/routes/app/role/page.js
@@ -0,0 +1,42 @@
+/*
+ * Projects4Me Copyright (c) 2017. Licensing : http://legal.projects4.me/LICENSE.txt. Do not remove this line
+ */
+
+import AppRoute from 'prometheus/routes/app';
+
+/**
+ *  This is the route to load a role selected by the user.
+ *
+ *  @class AppRolePageRoute
+ *  @namespace Prometheus.Routes
+ *  @module App.Role
+ *  @extends AppRoute
+ *  @author Rana Nouman <ranamnouman@gmail.com>
+ */
+export default class AppRolePageRoute extends AppRoute {
+    /**
+     * The model hook for this route. This function returns a role.
+     *
+     * @method model;
+     * @public
+     */
+     model(params) {
+        let _roleOptions = {
+            query: `((Role.id : ${params.role_id}))`
+        };
+
+        return this.store.query('role', _roleOptions);
+    }
+
+    /**
+     * This function is used to setup the controller for this route.
+     *
+     * @method setupController
+     * @param {Prometheus.Controllers.App.Role} controller the controller object for this route
+     * @param Object model
+     * @public
+     */   
+     setupController(controller, model) {
+        controller.set('model', model.objectAt(0));
+    }    
+}
