618b13e0d683ca0f47d83e75aeb94be1d2c643da
Noman Naeem <ranamnouman@gmail.com>
2024-01-08 03:20:23 +0500

    Update role controller

--- app/controllers/app/role.js
+++ app/controllers/app/role.js
@@ -2,7 +2,7 @@
  * Projects4Me Copyright (c) 2017. Licensing : http://legal.projects4.me/LICENSE.txt. Do not remove this line
  */
 
-import Controller from '@ember/controller';
+import PrometheusCreateController from 'prometheus/controllers/prometheus/create';
 import { tracked } from '@glimmer/tracking';
 
 /**
@@ -13,7 +13,20 @@ import { tracked } from '@glimmer/tracking';
  * @extends Ember.Controller
  * @author Rana Nouman <ranamnouman@gmail.com>
  */
-export default class AppRoleController extends Controller {
+export default class AppRoleController extends PrometheusCreateController {
+    /**
+     * This function is called on the initialization of the controller. In this function
+     * we're calling setupSchema method in order to generate schema, by analyzing metadata
+     * defined in the controller, that will be used to validate the form of the template.
+     *
+     * @method constructor
+     * @public
+     */
+    constructor() {
+        super(...arguments);
+        this.setupSchema();
+    }
+
     /**
      * This property is used to keep track the query for searching the role.
      *
@@ -25,9 +38,53 @@ export default class AppRoleController extends Controller {
     @tracked searchQuery = '';
 
     /**
+     * This object holds all of the information that we need to create our schema and also need to 
+     * render the template (in future).
+     * @property metadata
+     * @type Object
+     * @for AppRoleController
+     * @protected
+     */
+    metadata = {
+        sections: [
+            {
+                name: "roleCreate",
+                fields: [
+                    {
+                        name: "name",
+                        validations: {
+                            default: {
+                                type: "string",
+                                rules: [
+                                    {
+                                        name: "required"
+                                    }
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        name: "description",
+                        validations: {
+                            default: {
+                                type: "string",
+                                rules: [
+                                    {
+                                        name: "required"
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+
+    /**
      * This function return list of roles against the query given by the user.
      * 
-     * @method get
+     * @property filteredRoles
      * @return Array
      */
     get filteredRoles() {
@@ -35,5 +92,42 @@ export default class AppRoleController extends Controller {
             return role.name.toLowerCase().includes(this.searchQuery)
                 || role.name.includes(this.searchQuery);
         });
+    }
+
+    /**
+     * This function checks whether the current route is of role.page or not and then
+     * return boolean value.
+     * 
+     * @property isRolePage
+     * @return Boolean
+     */
+    get isRolePage() {
+        return this.router.currentRoute.localName === 'page';
+    }
+
+    /**
+     * This function returns the css classes for the div containing roles, depending upon
+     * the current route.
+     * 
+     * @property roleClass
+     * @return String
+     */
+    get rolesClass() {
+        return this.isRolePage
+            ? 'col-lg-2 col-md-12'
+            : 'col-md-12';
+    }
+
+    /**
+     * This function returns the css classes for the role cards, depending upon
+     * the current route.
+     * 
+     * @property roleCardClass
+     * @return String
+     */
+    get roleCardClass() {
+        return this.isRolePage
+            ? 'col-lg-12 col-md-3 col-sm-6 col-xs-6'
+            : 'col-xs-6 col-sm-6 col-lg-2 col-md-3';
     }
 }
