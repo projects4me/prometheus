de95f5c469a893566373e2324aa7981f93608da3
Noman Naeem <ranamnouman@gmail.com>
2024-01-11 17:10:33 +0500

    Update testing

--- tests/acceptance/steps/role/edit-role-fields-steps.js
+++ tests/acceptance/steps/role/edit-role-fields-steps.js
@@ -1,5 +1,5 @@
 import steps from '../steps';
-import { click, fillIn } from '@ember/test-helpers';
+import { click } from '@ember/test-helpers';
 
 export const when = function () {
     return [
@@ -8,19 +8,6 @@ export const when = function () {
                 let roles = document.querySelectorAll('.role-card');
                 let roleCardEl = roles[roleIndex - 1];
                 await click(roleCardEl);
-            }
-        },
-        {
-            "User clicks on edit button to change role $fieldName": (assert) => async function (fieldName) {
-                let editBtn = document.querySelector(`[data-field="role.${fieldName}"] [data-btn="edit"]`);
-                await click(editBtn);
-            }
-        },
-        {
-            "User enters $value in role $fieldName $fieldType field": (assert) => async function (value, fieldName, fieldType) {
-                let el = document.querySelector(`[data-field="role.${fieldName}"] ${fieldType}`);
-
-                await fillIn(el, value);
             }
         }
     ];
