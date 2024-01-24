fbf850f8b0fbda20b73be994db627b6cdfae0835
Noman Naeem <ranamnouman@gmail.com>
2024-01-24 12:12:13 +0500

    Add `permission-options` component to render options of the different modules of the system.

---
+++ app/components/role/permission-options.js
@@ -0,0 +1,17 @@
+/*
+ * Projects4Me Copyright (c) 2017. Licensing : http://legal.projects4.me/LICENSE.txt. Do not remove this line
+ */
+
+import Component from '@glimmer/component';
+
+/**
+ * This component is used to render dropdown containing the list of available options
+ * that the permission have.
+ *
+ * @class RolePermissionOptionsComponent
+ * @namespace Prometheus.Components
+ * @extends Ember.Component
+ * @author Rana Nouman <ranamnouman@gmail.com>
+ */
+export default class RolePermissionOptionsComponent extends Component {
+}
