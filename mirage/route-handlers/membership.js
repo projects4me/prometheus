9a7790a1605d03ed043f48515eec918ef2ab6cf1
Noman Naeem <ranamnouman@gmail.com>
2024-02-27 16:38:32 +0500

    Add routehandler for membership model

--- mirage/route-handlers/membership.js
+++ mirage/route-handlers/membership.js
@@ -1,6 +1,19 @@
+import getValueFromQuery from '../helpers/get-value-from-query';
 import getRequestData from '../helpers/parse-request';
 
 export function register(server, ctx) {
+    server.get('/membership', (schema, request) => {
+        let memberships = schema.memberships.all();
+        let query = request.queryParams.query;
+        let fieldName = ctx.get('requestQuery')?.membership;
+        let value = getValueFromQuery(fieldName, query);
+
+        if (fieldName) {
+            memberships = schema.memberships.where({ [fieldName]: value });
+        }
+        return memberships;
+    });
+
     server.post('membership', (schema, request) => {
         let requestData = getRequestData(request);
         let membership = server.create('membership', requestData.attributes);
