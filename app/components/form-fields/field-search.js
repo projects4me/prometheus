96b611f2dde2c49f2ef22205232a830bfb550b6c
Noman Naeem <ranamnouman@gmail.com>
2024-02-27 16:34:02 +0500

    Update field search component

--- app/components/form-fields/field-search.js
+++ app/components/form-fields/field-search.js
@@ -15,8 +15,27 @@ import { action } from '@ember/object';
  * @author Rana Nouman <ranamnouman@gmail.com>
  */
 export default class FormFieldsFieldSearchComponent extends Component {
+
+    /**
+     * This function is triggered which user enters some input.
+     * 
+     * @method onInput
+     * @param {KeyboardEvent} event 
+     */
     @action onInput(event) {
         this.query = event.target.value;
-        debounce(this, this.args.searchFunction, this.query, 200);
+        debounce(this, this.searchFunction, this.query, 200);
+    }
+
+    /**
+     * A getter property that retrieves the search function passed as an argument,
+     * or returns a default search function that always returns true if not provided.
+     * 
+     * @property {Function} searchFunction
+     * @type {Function}
+     * @returns {Function}
+     */
+    get searchFunction() {
+        return this.args.searchFunction ?? (() => true);
     }
 }
