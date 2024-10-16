a39d52518d901f88a99c49287ea68aeace55fbff
Noman Naeem <ranamnouman@gmail.com>
2024-10-16 18:36:17 +0500

    Update integration test for `initialize-doughnut-chart` modifier

--- tests/integration/modifiers/initialize-doughnut-chart-test.js
+++ tests/integration/modifiers/initialize-doughnut-chart-test.js
@@ -12,8 +12,18 @@ module('Integration | Modifier | initialize-doughnut-chart', function (hooks) {
 
     // Replace this with your real tests.
     test('it attach doughnut chart with canvas', async function (assert) {
+        let data = {
+            labels: ['closed', 'open'],
+            datasets: [{
+                data: [1, 1],
+                backgroundColor: ['#508874', '#A2EDD2']
+            }],
+        }
+
+        this.set('data', data);
+
         await render(hbs`
-            <canvas {{initialize-doughnut-chart}}></canvas>
+            <canvas {{initialize-doughnut-chart data=this.data}}></canvas>
         `);
 
         let modifierAttachedOrNot = (this.element.querySelector('canvas').$chartjs);
