5b3fce97666a4c87103a85cacc8b4d62a1b1887a
Noman Naeem <ranamnouman@gmail.com>
2025-03-24 13:32:11 +0500

    Disable button until the comment is saved

--- app/controllers/app/project/issue/page.js
+++ app/controllers/app/project/issue/page.js
@@ -130,16 +130,6 @@ export default class AppProjectIssuePageController extends PrometheusController.
     }
 
     /**
-     * The comments from the comment box
-     *
-     * @property comment
-     * @type Array
-     * @for Page
-     * @public
-     */
-    comment = null;
-
-    /**
      * This is a task to handle file uploading
      *
      * @param handleUpload
@@ -189,6 +179,7 @@ export default class AppProjectIssuePageController extends PrometheusController.
      *
      * @param issue
      * @param comment
+     * @return {Promise}
      * @private
      */
     _createComment(issue, content) {
@@ -201,12 +192,12 @@ export default class AppProjectIssuePageController extends PrometheusController.
             comment: content,
         });
 
-        comment.save().then(function (savedComment) {
+        Logger.debug('-Prometheus.Controllers.App.Project.Issue.Page::_createComment');
+
+        return comment.save().then(function (savedComment) {
             issue.get('comments').pushObject(savedComment);
             _self.trigger('clearContents');
         });
-
-        Logger.debug('-Prometheus.Controllers.App.Project.Issue.Page::_createComment');
     }
 
     /**
@@ -574,6 +565,17 @@ export default class AppProjectIssuePageController extends PrometheusController.
         Logger.debug('-App.Project.Issue.PageController->logTime');
     }
 
+    /**
+     * This action is used to save a comment for a given issue. If the issue does not have an associated
+     * conversation room, a new conversation room is created first. Once the conversation room is available,
+     * the comment is created and saved.
+     *
+     * @method saveComment
+     * @param {Prometheus.Models.Issue} issue - The issue object for which the comment is being saved.
+     * @param {Prometheus.Models.Comment} comment - The content of the comment to be saved.
+     * @return {Promise} - A promise that resolves when the comment is successfully saved.
+     * @public
+     */    
     @action saveComment(issue, comment) {
         Logger.debug('Prometheus.Controller.App.Project.Issue.Page::saveComment');
 
@@ -593,19 +595,15 @@ export default class AppProjectIssuePageController extends PrometheusController.
                 projectName: issue.get('project.name'),
                 issueId: issue.get('id')
             });
-            Logger.debug(newConversation);
-            // Save it
-            newConversation.save().then(function (conversation) {
+            Logger.debug('-Prometheus.Controller.App.Project.Issue.Page::saveComment');
+            return newConversation.save().then(function (conversation) {
                 issue.set('conversationRoomId', conversation.get('id'))
-                _self._createComment(issue, comment);
-                _self.set('comment', null);
+               return _self._createComment(issue, comment);
             });
         } else {
-            _self._createComment(issue, comment);
-            _self.set('comment', null);
+            Logger.debug('-Prometheus.Controller.App.Project.Issue.Page::saveComment');
+            return _self._createComment(issue, comment);
         }
-
-        Logger.debug('-Prometheus.Controller.App.Project.Issue.Page::saveComment');
     }
 
     /**
